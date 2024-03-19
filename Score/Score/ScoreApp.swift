ca3a8c539671416e692567a0af4d655d87917549
Hyemin Heo <heohm128@gmail.com>
2024-03-19 18:25:29 +0900

    [Chore] 프로젝트 세팅

---
+++ Score/Score/ScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreApp.swift
+//  Score
+//
+//  Created by sole on 3/19/24.
+//
+
+import SwiftUI
+
+@main
+struct ScoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
