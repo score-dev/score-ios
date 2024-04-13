e3836681af601f61c428419b02a1e067a773d014
Hyemin Heo <heohm128@gmail.com>
2024-04-14 07:48:57 +0900

    #26 [Feat] ProfileEditFeature 구현

---
+++ Score/Score/Reducer/ProfileEditFeature.swift
@@ -0,0 +1,89 @@
+//
+//  ProfileEditFeature.swift
+//  Score
+//
+//  Created by sole on 4/12/24.
+//
+
+import ComposableArchitecture
+import PhotosUI
+import SwiftUI
+
+@Reducer
+struct ProfileEditFeature {
+    struct State: Equatable {
+        var displayedUser: User?
+    
+        var displayedProfileImage: Image?
+        
+        @BindingState var displayedNickName: String
+        @BindingState var displayedHeight: String
+        @BindingState var displayedWeight: String
+        @BindingState var displayedSex: Sex
+        @BindingState var displayedSchool: String
+        @BindingState var displayedGrade: Int
+        
+        @BindingState var isPresentingEditGradeSheet: Bool
+        @BindingState var isPresentingEditSchoolSheet: Bool
+
+    }
+    
+    enum Action: BindableAction {
+        case binding(BindingAction<State>)
+
+        case schoolEditButtonTapped
+        case gradeEditButtonTapped
+        case sexSelectButtonTapped(Sex)
+        case editDoneButtonTapped
+        
+        case photoSelectChanging(PhotosPickerItem?)
+        case imageUpdating(Image?)
+    }
+    
+    var body: some ReducerOf<Self> {
+        BindingReducer()
+        
+        Reduce { state, action in
+            switch action {
+            case .schoolEditButtonTapped:
+                return .none
+                
+            case .gradeEditButtonTapped:
+                // presenting sheet
+                state.isPresentingEditGradeSheet = true
+                return .none
+                
+            case .sexSelectButtonTapped(let sex):
+                state.displayedSex = sex
+                return .none
+                
+            case .editDoneButtonTapped:
+                // to update the remote database
+                return .none
+                
+            case .photoSelectChanging(let photosPickerItem):
+                guard let photosPickerItem
+                else {
+                    return .send(.imageUpdating(nil))
+                }
+                
+                return .run { send in
+                    let image = try await photosPickerItem.loadTransferable(type: Image.self)
+                    await send(.imageUpdating(image))
+                } catch: { error, send in
+                    await send(.imageUpdating(nil))
+                }
+                
+            case .imageUpdating(let image):
+                guard let image
+                else { return .none }
+                state.displayedProfileImage = image
+                return .none
+                
+            case .binding(_):
+                return .none
+            }
+        }
+    }
+}
+
