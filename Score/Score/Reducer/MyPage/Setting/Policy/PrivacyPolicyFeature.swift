f6998a3df5688af05401adc6339c47dd2bd2fdc4
Hyemin Heo <heohm128@gmail.com>
2024-04-24 05:16:27 +0900

    #26 [Feat] PrivacyPolicyFeature 구현

---
+++ Score/Score/Reducer/MyPage/Setting/Policy/PrivacyPolicyFeature.swift
@@ -0,0 +1,32 @@
+//
+//  PrivacyPolicyFeature.swift
+//  Score
+//
+//  Created by sole on 4/19/24.
+//
+
+import ComposableArchitecture
+
+@Reducer
+struct PrivacyPolicyFeature {
+    @Dependency(\.dismiss) var dismiss
+    
+    struct State: Equatable {
+        
+    }
+    
+    enum Action {
+        case dismissButtonTapped
+    }
+    
+    var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+            case .dismissButtonTapped:
+                return .run { send in
+                    await self.dismiss()
+                }
+            }
+        }
+    }
+}
