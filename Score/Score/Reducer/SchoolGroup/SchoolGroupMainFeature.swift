c6c9d9f4faaec8e01bfd6475ae4ed6ec6b591f50
Hyemin Heo <heohm128@gmail.com>
2024-04-24 10:04:35 +0900

    #37 [Add] SchoolGroupMainFeature 추가

---
+++ Score/Score/Reducer/SchoolGroup/SchoolGroupMainFeature.swift
@@ -0,0 +1,27 @@
+//
+//  SchoolGroupMainFeature.swift
+//  Score
+//
+//  Created by sole on 4/24/24.
+//
+
+import ComposableArchitecture
+
+@Reducer
+struct SchoolGroupMainFeature {
+    struct State: Equatable {
+        
+    }
+    
+    enum Action {
+        
+    }
+    
+    var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+                
+            }
+        }
+    }
+}
