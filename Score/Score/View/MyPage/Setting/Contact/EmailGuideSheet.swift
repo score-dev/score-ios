af3489e4f9a70b65f82c56847f7a5d5550c2ac83
Hyemin Heo <heohm128@gmail.com>
2024-04-24 21:17:07 +0900

    #39 [Feat] viewStore 구조 수정

--- Score/Score/View/MyPage/Setting/Contact/EmailGuideSheet.swift
+++ Score/Score/View/MyPage/Setting/Contact/EmailGuideSheet.swift
@@ -12,33 +12,38 @@ import SwiftUI
 
 struct EmailGuideSheet: View {
     let store: StoreOf<ContactFeature>
+    @ObservedObject var viewStore: ViewStoreOf<ContactFeature>
+    
+    init(store: StoreOf<ContactFeature>) {
+        self.store = store
+        self.viewStore = ViewStore(store,
+                                   observe: { $0 })
+    }
     
     var body: some View {
-        WithViewStore(store, observe: { $0 }) { viewStore in
-            VStack(alignment: .leading) {
-                HStack {
-                    Spacer()
-                    
-                    DismissButton(style: .close) {
-                        viewStore.send(.dismissButtonTapped)
-                    }
-                }
-                
-                Text("문의 사항을 알려주세요.")
-                    .pretendard(.title)
-                
+        VStack(alignment: .leading) {
+            HStack {
                 Spacer()
                 
-                SCButton(style: .primary) {
+                DismissButton(style: .close) {
                     viewStore.send(.dismissButtonTapped)
-                } label: {
-                    Text("확인")
-                        .frame(maxWidth: .infinity)
                 }
             }
-            .padding(.vertical, 24)
-            .layout()
+            
+            Text("문의 사항을 알려주세요.")
+                .pretendard(.title)
+            
+            Spacer()
+            
+            SCButton(style: .primary) {
+                viewStore.send(.dismissButtonTapped)
+            } label: {
+                Text("확인")
+                    .frame(maxWidth: .infinity)
+            }
         }
+        .padding(.vertical, 24)
+        .layout()
     }
 }
 
