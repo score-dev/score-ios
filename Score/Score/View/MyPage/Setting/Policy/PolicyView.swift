1ff3d8deecdef083f103be2a0b0bee0567c681be
Hyemin Heo <heohm128@gmail.com>
2024-04-24 21:17:26 +0900

    #39 [Delete] viewStore 제거

--- Score/Score/View/MyPage/Setting/Policy/PolicyView.swift
+++ Score/Score/View/MyPage/Setting/Policy/PolicyView.swift
@@ -14,53 +14,47 @@ struct PolicyView: View {
     private let navigationTitles = Navigation.Policy.self
     let store: StoreOf<PolicyFeature>
     
+    // state에 접근하지 않기 때문에 ViewStore를 사용하지 않습니다. 
     var body: some View {
-        WithViewStore(store,
-                      observe: { $0 }) { viewStore in
-            VStack(alignment: .leading) {
-                Button {
-                    viewStore.send(.servicePolicyButtonTapped)
-                } label: {
-                    Text(navigationTitles.service.rawValue)
-                        .settingRowModifier()
-                }
-                Button {
-                    viewStore.send(.privacyPolicyButtonTapped)
-                } label: {
-                    Text(navigationTitles.privacy.rawValue)
-                        .settingRowModifier()
-                }
-                
-                Spacer()
+        VStack(alignment: .leading) {
+            Button {
+                store.send(.servicePolicyButtonTapped)
+            } label: {
+                Text(navigationTitles.service.rawValue)
+                    .settingRowModifier()
             }
-            .layout()
-            .scNavigationBar(style: .vertical) {
-                DismissButton(style: .chevron) {
-                    viewStore.send(.dismissButtonTapped)
-                }
-                
-                Text("이용약관")
+            Button {
+                store.send(.privacyPolicyButtonTapped)
+            } label: {
+                Text(navigationTitles.privacy.rawValue)
+                    .settingRowModifier()
             }
-            .fullScreenCover(
-                store: store.scope(
-                state: \.$destination,
-                action: \.destination
-            ),
-            state: /PolicyFeature.Destination.State.service,
-            action: PolicyFeature.Destination.Action.service
-            ) { store in
-                ServicePolicyView(store: store)
+            
+            Spacer()
+        }
+        .layout()
+        .scNavigationBar(style: .vertical) {
+            DismissButton(style: .chevron) {
+                store.send(.dismissButtonTapped)
             }
-            .fullScreenCover(
-                store: store.scope(
-                state: \.$destination,
-                action: \.destination
-            ),
-            state: /PolicyFeature.Destination.State.privacy,
-            action: PolicyFeature.Destination.Action.privacy
-            ) { store in
-                PrivacyPolicyView(store: store)
-            }
+            
+            Text("이용약관")
+        }
+        .fullScreenCover(
+            store: store.scope(
+                state: \.$destination.service,
+                action: \.destination.service
+            )
+        ) { store in
+            ServicePolicyView(store: store)
+        }
+        .fullScreenCover(
+            store: store.scope(
+                state: \.$destination.privacy,
+                action: \.destination.privacy
+            )
+        ) { store in
+            PrivacyPolicyView(store: store)
         }
     }
 }
