d2a948c5b56808237f8ed80b9d24e2842a926d79
Hyemin Heo <heohm128@gmail.com>
2024-05-16 22:10:03 +0900

    #26 [Style] imagePlaceHolder 추가

---
+++ Score/Score/Extension/Image+.swift
@@ -0,0 +1,59 @@
+//
+//  Image+.swift
+//  Score
+//
+//  Created by sole on 4/27/24.
+//
+
+import SwiftUI
+
+//MARK: - Image+imagePlaceHolder
+
+extension Image {
+    
+    //MARK: - imagePlaceHolder
+    
+    /// image 플레이스 홀더가 필요한 사진의 경우에 사용합니다.
+    /// 플레이스 홀더를 원형의 size*size로 생성합니다.
+    @ViewBuilder
+    func imagePlaceHolder(size: CGFloat) -> some View {
+        self
+            .resizable()
+            .frame(width: 130,
+                   height: 130)
+            .clipShape(Circle())
+            .background {
+                Circle()
+                    .frame(width: size,
+                           height: size)
+                    .foregroundStyle(
+                        Color.brandColor(color: .gray2)
+                    )
+            }
+            .frame(width: size,
+                   height: size)
+    }
+}
+
+extension Image? {
+    
+    @ViewBuilder
+    func imagePlaceHolder(size: CGFloat) -> some View {
+        let image: Image = self ?? Image("")
+        image
+            .resizable()
+            .frame(width: 130,
+                   height: 130)
+            .clipShape(Circle())
+            .background {
+                Circle()
+                    .frame(width: size,
+                           height: size)
+                    .foregroundStyle(
+                        Color.brandColor(color: .gray2)
+                    )
+            }
+            .frame(width: size,
+                   height: size)
+    }
+}
