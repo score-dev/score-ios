ad448040c030ee48c5a46dab0a433d0ded015edf
Hyemin Heo <heohm128@gmail.com>
2024-05-23 18:51:59 +0900

    #46 [Add] AuthToken 추가

---
+++ Score/Score/Model/AuthToken.swift
@@ -0,0 +1,13 @@
+//
+//  AuthToken.swift
+//  Score
+//
+//  Created by sole on 5/21/24.
+//
+
+import Foundation
+
+struct AuthToken: Equatable {
+    let accessToken: String
+    let refreshToken: String
+}
