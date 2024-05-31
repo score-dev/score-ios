250814f887335e97af1c37820c5d3d10c895f72c
Hyemin Heo <heohm128@gmail.com>
2024-06-01 01:49:09 +0900

    #49 [Feat] AppDelegate 추가

---
+++ Score/Score/AppDelegate.swift
@@ -0,0 +1,46 @@
+//
+//  AppDelegate.swift
+//  Score
+//
+//  Created by sole on 5/28/24.
+//
+
+import Foundation
+import GoogleSignIn
+import KakaoSDKAuth
+import KakaoSDKCommon
+
+//MARK: - AppDelegate
+
+class AppDelegate: NSObject,
+                   UIApplicationDelegate {
+    private let apiKeys = Constants.APIKey.self
+    
+    //MARK: - application(:didFinishLaunchingWithOptions:)
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+    ) -> Bool {        
+        /// KakaoSDK 초기화
+        KakaoSDK.initSDK(
+            appKey: self.apiKeys.Kakao.appID.getValueFromBundle() ?? "none"
+        )
+            
+        /// Google Client ID 초기화
+        GIDSignIn.sharedInstance.configuration = .init(
+            clientID: self.apiKeys.Google.clientID.getValueFromBundle() ?? "none"
+        )
+        
+        return false
+    }
+    
+    //MARK: - application(:open:options:)
+    
+    func application(
+      _ app: UIApplication,
+      open url: URL,
+      options: [UIApplication.OpenURLOptionsKey : Any] = [:]
+    ) -> Bool {
+        return GIDSignIn.sharedInstance.handle(url)
+    }
+}
