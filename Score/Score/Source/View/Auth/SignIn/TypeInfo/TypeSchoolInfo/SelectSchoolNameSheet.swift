0def5574d32a8c7db1628f526b3ac1201d41d2be
Hyemin Heo <heohm128@gmail.com>
2024-07-08 00:01:03 +0900

    #54 [Add] SelectSchoolNameSheet 추가

---
+++ Score/Score/Source/View/Auth/SignIn/TypeInfo/TypeSchoolInfo/SelectSchoolNameSheet.swift
@@ -0,0 +1,18 @@
+//
+//  SelectSchoolNameSheet.swift
+//  Score
+//
+//  Created by sole on 7/7/24.
+//
+
+import SwiftUI
+
+struct SelectSchoolNameSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SelectSchoolNameSheet()
+}
