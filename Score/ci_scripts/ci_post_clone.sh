3a163c448cbb01a31daec7aff3db90daf128e08c
Hyemin Heo <97100404+soletree@users.noreply.github.com>
2024-07-09 23:01:29 +0900

    [Add] ci_scripts 추가

---
+++ Score/ci_scripts/ci_post_clone.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+#  ci_post_clone.sh
+#  Score
+#
+#  Created by sole on 7/9/24.
+#  
+
+# Install CocoaPods using Homebrew.
+brew install cocoapods
+
+# Install dependencies you manage with CocoaPods.
+pod install
+
+# *.xconfig 파일이 생성될 폴더 경로
+FOLDER_PATH="/Volumes/workspace/repository/Score/Resource"
+# *.xconfig 파일 이름
+CONFIG_FILENAME="Secrets.xcconfig"
+
+# *.xconfig 파일의 전체 경로 계산
+CONFIG_FILE_PATH="$FOLDER_PATH/$CONFIG_FILENAME"
+
+# 환경 변수에서 값을 가져와서 *.xconfig 파일에 추가하기
+echo "SERVER_BASE_URL = $SERVER_BASE_URL" >> "$CONFIG_FILE_PATH"
+
+echo "NAVER_CLIENT_ID = $NAVER_CLIENT_ID" >> "$CONFIG_FILE_PATH"
+echo "NAVER_SECRET_KEY = $NAVER_SECRET_KEY" >> "$CONFIG_FILE_PATH"
+
+echo "KAKAO_APP_ID = $KAKAO_APP_ID" >> "$CONFIG_FILE_PATH"
+
+echo "GOOGLE_CLIENT_ID = $GOOGLE_CLIENT_ID" >> "$CONFIG_FILE_PATH"
+echo "GOOGLE_REVERSED_CLIENT_ID = $GOOGLE_REVERSED_CLIENT_ID" >> "$CONFIG_FILE_PATH"
+echo "GOOGLE_SERVER_CLIENT_ID = $GOOGLE_SERVER_CLIENT_ID" >> "$CONFIG_FILE_PATH"
+
---
+++ Score/ci_scripts/ci_pre_xcodebuild.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+#  ci_pre_xcodebuild.sh
+#  Score
+#
+#  Created by sole on 7/9/24.
+#  
+
+# macro error 처리
+defaults write com.apple.dt.Xcode IDESkipMacroFingerprintValidation -bool YES
+
+
