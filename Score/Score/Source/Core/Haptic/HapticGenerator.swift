6c5fae3a1edf5c132aa17c1b6506fd3889029e88
Hyemin Heo <heohm128@gmail.com>
2024-10-20 20:18:44 +0900

    [Feat] HapticGenerator 추가

---
+++ Score/Score/Source/Core/Haptic/HapticGenerator.swift
@@ -0,0 +1,12 @@
+//
+//  HapticGenerator.swift
+//  Score
+//
+//  Created by sole on 10/20/24.
+//
+
+import UIKit
+
+enum HapticGenerator {
+    static let shared: UIImpactFeedbackGenerator = .init(style: .medium)
+}
